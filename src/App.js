6b39bcb096ecb12e8c73d271dde86963e1ad1168
aamyear <aamyear@gmail.com>
2021-07-07 15:26:12 +0400

    css grid structure created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
-import SampleComponent from "./components/SampleComponent";
+import Header from "./components/Structural/Header";
+import Content from "./components/Structural/Content.js";
+import Footer from "./components/Structural/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Clean React</h1>
-      <SampleComponent />
+      <div className="header">
+        <Header />
+      </div>
+      <div className="container">
+        <Content />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
