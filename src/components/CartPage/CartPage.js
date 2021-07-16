df6b06a80d9c5afb20fc731213ae7ac49b821bcd
aamyear <aamyear@gmail.com>
2021-07-16 09:00:06 +0400

    cart fetching product details from context

--- src/components/CartPage/CartPage.js
+++ src/components/CartPage/CartPage.js
@@ -2,24 +2,66 @@ import React from "react";
 import "./CartPage.css";
 
 import { useCart } from "../../contexts/CartContext";
+import { useProductData } from "../../contexts/ProductDataContext";
 
 function CartPage() {
-  const { cartValue } = useCart();
+  const { cartState, cartDispatch } = useCart();
+  const { productData } = useProductData();
+
+  // console.log(productData);
+
+  const cartItemKeys = Object.keys(cartState);
 
   return (
     <div className="cart-page">
-      <div class="title">
+      <div className="title">
         <h1>SHOPPING CART</h1>
       </div>
-      <div class="items">
+      <div className="items">
+        {cartItemKeys.map((key) => {
+          const { units } = cartState[key];
+          const { title } = productData[key];
+          const { base, card } = productData[key].images;
+          return (
+            <div key={Math.random().toFixed(5) * 100000} className="item">
+              <div className="image">
+                <img src={base + card.path} alt={card} />
+              </div>
+              <div className="title">{title}</div>
+              <div className="count">
+                <i
+                  onClick={() => cartDispatch({ type: "increaseCountInCart" })}
+                  className="fas fa-chevron-up"
+                ></i>
+                {units}
+                <i
+                  onClick={() => cartDispatch({ type: "decreaseCountInCart" })}
+                  className="fas fa-chevron-down"
+                ></i>
+              </div>
+              <div className="cost">Cost</div>
+            </div>
+          );
+        })}
+
         <div className="item">
           <div className="image">Image</div>
           <div className="title">Title</div>
-          <div className="count">Count</div>
+          <div className="count">
+            <i
+              onClick={() => cartDispatch({ type: "increaseCountInCart" })}
+              className="fas fa-chevron-up"
+            ></i>
+            100
+            <i
+              onClick={() => cartDispatch({ type: "decreaseCountInCart" })}
+              className="fas fa-chevron-down"
+            ></i>
+          </div>
           <div className="cost">Cost</div>
         </div>
       </div>
-      <div class="total">TOTAL </div>
+      <div className="total">TOTAL </div>
     </div>
   );
 }
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -3,11 +3,40 @@ import React, { createContext, useContext, useReducer } from "react";
 const CartContext = createContext(null);
 export { CartContext };
 
+const initialCart = {
+  keyboard001: { units: 5, inWishlist: false },
+  keyboard002: { units: 5, inWishlist: false },
+  keyboard003: { units: 5, inWishlist: false },
+};
+
 export function CartProvider({ children }) {
   const cartValue = "I am the cart";
 
+  const [cartState, cartDispatch] = useReducer((cartState, action) => {
+    // local states here
+
+    switch (action.type) {
+      case "increaseCountInCart":
+        console.log("Item increased in cart");
+        return cartState;
+
+      case "decreaseCountInCart":
+        console.log("Item decreased in cart");
+        return cartState;
+
+      case "removeItemFromCart":
+        console.log("Item removed from cart");
+        return cartState;
+
+      default:
+        break;
+    }
+
+    console.log("Current cart:", cartState);
+  }, initialCart);
+
   return (
-    <CartContext.Provider value={{ cartValue }}>
+    <CartContext.Provider value={{ cartValue, cartState, cartDispatch }}>
       {children}
     </CartContext.Provider>
   );
