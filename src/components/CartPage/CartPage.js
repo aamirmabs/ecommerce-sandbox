4841bf6a894eea1f4337dc6f83be91ed139a24ed
aamyear <aamyear@gmail.com>
2021-07-22 21:02:59 +0400

    cart remove item from list if units in cart are 0

--- src/components/CartPage/CartPage.js
+++ src/components/CartPage/CartPage.js
@@ -22,54 +22,46 @@ function CartPage() {
           const { units } = cartState[key];
           const { title } = productData[key];
           const { base, card } = productData[key].images;
-          return (
-            <div key={Math.random().toFixed(5) * 100000} className="item">
-              <div className="image">
-                <img src={base + card.path} alt={card} />
-              </div>
-              <div className="title">{title}</div>
-              <div className="count">
-                <i
-                  onClick={() =>
-                    cartDispatch({ type: "increaseCountInCart", payload: key })
-                  }
-                  className="fas fa-chevron-up"
-                ></i>
-                {units}
-                <i
-                  onClick={() =>
-                    cartDispatch({ type: "decreaseCountInCart", payload: key })
-                  }
-                  className="fas fa-chevron-down"
-                ></i>
-                <i
-                  onClick={() =>
-                    cartDispatch({ type: "removeFromCart", payload: key })
-                  }
-                  className="fas fa-times-circle"
-                ></i>
-              </div>
-              <div className="cost">Cost</div>
-            </div>
-          );
-        })}
 
-        <div className="item">
-          <div className="image">Image</div>
-          <div className="title">Title</div>
-          <div className="count">
-            <i
-              onClick={() => cartDispatch({ type: "increaseCountInCart" })}
-              className="fas fa-chevron-up"
-            ></i>
-            100
-            <i
-              onClick={() => cartDispatch({ type: "decreaseCountInCart" })}
-              className="fas fa-chevron-down"
-            ></i>
-          </div>
-          <div className="cost">Cost</div>
-        </div>
+          if (units > 0) {
+            return (
+              <div key={Math.random().toFixed(5) * 100000} className="item">
+                <div className="image">
+                  <img src={base + card.path} alt={card} />
+                </div>
+                <div className="title">{title}</div>
+                <div className="count">
+                  <i
+                    onClick={() =>
+                      cartDispatch({
+                        type: "increaseCountInCart",
+                        payload: key,
+                      })
+                    }
+                    className="fas fa-chevron-up"
+                  ></i>
+                  {units}
+                  <i
+                    onClick={() =>
+                      cartDispatch({
+                        type: "decreaseCountInCart",
+                        payload: key,
+                      })
+                    }
+                    className="fas fa-chevron-down"
+                  ></i>
+                  <i
+                    onClick={() =>
+                      cartDispatch({ type: "removeFromCart", payload: key })
+                    }
+                    className="fas fa-times-circle"
+                  ></i>
+                </div>
+                <div className="cost">Cost</div>
+              </div>
+            );
+          }
+        })}
       </div>
       <div className="total">TOTAL </div>
     </div>
