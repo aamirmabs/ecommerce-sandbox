a06c1da8687281bd75b9c358c9cbd9d94c200584
aamyear <aamyear@gmail.com>
2021-08-02 09:19:59 +0400

    fixed cart button to use AddToCartBtn on product display page

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -9,6 +9,7 @@ import { useProductData } from "../../contexts/ProductDataContext";
 function ProductPage(props) {
   const { productData } = useProductData();
   const product = productData.keyboard001;
+  const key = "keyboard001";
 
   let currentSlide = 0;
 
@@ -21,7 +22,11 @@ function ProductPage(props) {
       <div className="slider">
         <img src={slideSrc} alt={"image" + currentSlide} />
       </div>
-      <PrimaryDesc title={product.title} data={product.parameters} />
+      <PrimaryDesc
+        title={product.title}
+        data={product.parameters}
+        productKey={key}
+      />
       <SecondaryDesc data={product.description} />
     </div>
   );
