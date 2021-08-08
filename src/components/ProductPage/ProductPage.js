8bb0c061101bd65322028d16ce2e7620d1bb7cfa
aamyear <aamyear@gmail.com>
2021-08-08 17:30:21 +0400

    slider configured to work on product page

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -1,27 +1,33 @@
 import React from "react";
 import "./ProductPage.css";
 
+import Slider from "../Structural/Slider";
 import PrimaryDesc from "./PrimaryDesc";
 import SecondaryDesc from "./SecondaryDesc";
 
 import { useProductData } from "../../contexts/ProductDataContext";
 
 function ProductPage(props) {
+  const propps = {
+    key: `keyboard001`,
+  };
+
+  const { key } = propps;
+
   const { productData } = useProductData();
-  const product = productData.keyboard001;
-  const key = "keyboard001";
+  const product = productData[key];
 
-  let currentSlide = 0;
-
-  const sliderArray = product.images.slider;
   const slidePath = "/assets/images/products/keyboard001/";
-  const slideSrc = slidePath + sliderArray[currentSlide];
+  const sliderArray = product.images.slider;
 
   return (
     <div className="product-page">
-      <div className="slider">
-        <img src={slideSrc} alt={"image" + currentSlide} />
-      </div>
+      <Slider
+        containerClass="slider"
+        slideClass="slide"
+        base={slidePath}
+        files={sliderArray}
+      />
       <PrimaryDesc
         title={product.title}
         data={product.parameters}
