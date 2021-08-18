f4d6b8b6aa66de9121e76972875bbaf7269b5f41
aamyear <aamyear@gmail.com>
2021-08-18 18:36:34 +0400

    history block component created + wired with css + rendered on home page

---
+++ src/components/HistoryBlock/HistoryBlock.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./HistoryBlock.css";
+
+function HistoryBlock() {
+  return (
+    <div className="history-block">
+      <div className="section">
+        <div className="title center">Recently Viewed</div>
+        <h3>History</h3>
+      </div>
+    </div>
+  );
+}
+
+export default HistoryBlock;
