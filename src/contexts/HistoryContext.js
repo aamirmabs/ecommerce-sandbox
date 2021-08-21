92501078a68c1593b91124f9456c5d633cec004b
aamyear <aamyear@gmail.com>
2021-08-21 19:55:58 +0400

    history limit is controlled with a variable

--- src/contexts/HistoryContext.js
+++ src/contexts/HistoryContext.js
@@ -6,6 +6,7 @@ export { HistoryContext };
 
 export function HistoryProvider({ children }) {
   const { setRouter } = useRouter();
+  const maxHistoryLength = 5;
 
   const [history, setHistory] = useState({
     activeProduct: null,
@@ -27,25 +28,13 @@ export function HistoryProvider({ children }) {
     }
 
     // remove first value in array if length > 5
-    if (newProductHistory.length > 5) {
+    if (newProductHistory.length > maxHistoryLength) {
       newProductHistory.shift();
     }
 
     setHistory((oldHistory) => {
       return { ...oldHistory, productHistory: newProductHistory };
     });
-
-    // if (newProductHistory.includes(key) === false) {
-    //   newProductHistory.length > 5 && newProductHistory.shift();
-    //   console.log("25");
-    //   setHistory((oldHistory) => {
-    //     newProductHistory.push(key);
-    //     return {
-    //       ...oldHistory,
-    //       productHistory: newProductHistory,
-    //     };
-    //   });
-    // }
   };
 
   return (
