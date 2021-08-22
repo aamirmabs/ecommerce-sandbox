5cd0ed0824d5da8414c357c81eee73bff187453e
aamyear <aamyear@gmail.com>
2021-08-22 18:52:18 +0400

    keys accessed and rendered from history.productHistory

--- src/components/Structural/VisitHistory.js
+++ src/components/Structural/VisitHistory.js
@@ -3,11 +3,21 @@ import { useHistory } from "../../contexts/HistoryContext";
 
 function VisitHistory() {
   const { history, setHistory, displayProduct, trackProduct } = useHistory();
+  const { productHistory } = history;
+  const isHistoryAvailable = productHistory.length > 0 ? true : false;
+  // history{
+  //   activeProduct: null,
+  //   productHistory: [],
+  // }
 
   return (
     <div className="visit-history">
       <div className="visit-history-title">Recently Viewed</div>
       <div className="history-container">
+        {productHistory.map((historyItem) => {
+          return <div className="history-card">{historyItem}</div>;
+        })}
+
         <div className="history-card">
           <div className="image">Image</div>
           <div className="title">Title</div>
